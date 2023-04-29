0feb6a5e14f9409d32b95110dc19bdfeb5641c67
InfiniteCod3 <91837651+InfiniteCod3@users.noreply.github.com>
2023-04-29 08:32:47 +0200

    lol

--- functions/challenge.js
+++ functions/challenge.js
@@ -50,39 +50,40 @@ const CHALLENGE_PAGE = `
 
 // A function that verifies if a cookie value is valid by comparing it with the secret key.
 function verifyCookie(cookie) {
-  // Split the cookie string by semicolons and find the one that starts with the cookie name.
-  const value = cookie.split(";").find((part) => part.trim().startsWith(`${COOKIE_NAME}=`));
+    // Split the cookie string by semicolons and find the one that starts with the cookie name.
+    const value = cookie.split(";").find((part) => part.trim().startsWith(`${COOKIE_NAME}=`));
 
-  // If the value exists, extract the hash from it and compare it with the secret key.
-  if (value) {
-    const hash = value.split("=")[1];
-    return hash === SECRET_KEY;
-  }
+    // If the value exists, extract the hash from it and compare it with the secret key.
+    if (value) {
+        const hash = value.split("=")[1];
+        return hash === SECRET_KEY;
+    }
 
-  // If the value does not exist, return false.
-  return false;
+    // If the value does not exist, return false.
+    return false;
 }
 
 // A function that handles requests to the main page and performs the cookie and JavaScript challenges.
 async function handleRequest(request) {
-  // Get the cookie value from the request headers.
-  const cookie = request.headers.get("Cookie");
+    // Get the cookie value from the request headers.
+    const cookie = request.headers.get("Cookie");
 
-  // If the cookie exists and is valid, return the main page content.
-  if (cookie && verifyCookie(cookie)) {
-    return fetch(request);
-  }
+    // If the cookie exists and is valid, redirect to mitigated.sbs.
+    if (cookie && verifyCookie(cookie)) {
+        return Response.redirect("https://mitigated.sbs", 302);
+    }
 
-  // If the cookie does not exist or is invalid, return the JavaScript challenge page with a new cookie.
-  return new Response(CHALLENGE_PAGE, {
-    headers: {
-      "Content-Type": "text/html",
-      // Set the cookie with a random string as a placeholder for the challenge result.
-      "Set-Cookie": `${COOKIE_NAME}=${Math.random().toString(36).substring(2)}; path=/`,
-    },
-  });
+
+    // If the cookie does not exist or is invalid, return the JavaScript challenge page with a new cookie.
+    return new Response(CHALLENGE_PAGE, {
+        headers: {
+            "Content-Type": "text/html",
+            // Set the cookie with a random string as a placeholder for the challenge result.
+            "Set-Cookie": `${COOKIE_NAME}=${Math.random().toString(36).substring(2)}; path=/`,
+        },
+    });
 }
 
 addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
+    event.respondWith(handleRequest(event.request));
 });
